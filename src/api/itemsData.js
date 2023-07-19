ff34aeabec23d2cf572bdf71fffeb65ef5f91143
amirahnasihah <amirahnasihah97@gmail.com>
2023-07-19 15:06:05 +0800

    rm process env

--- src/api/itemsData.js
+++ src/api/itemsData.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-const API_KEY = process.env.REACT_APP_API_KEY;
 
 export default axios.create({
-  baseURL: `https://${API_KEY}.mockapi.io`,
+  baseURL: `https://64188b7a29e7e36438eb6466.mockapi.io`,
 });
